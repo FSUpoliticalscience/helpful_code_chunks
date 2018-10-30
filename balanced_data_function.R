4043c1a6b6317b8152b724338ddc518e1861df51
Kimberly Fruge <kfruge@fsu.edu>
2018-10-30 12:31:17 -0400

    Update balanced_data_function.R

--- balanced_data_function.R
+++ balanced_data_function.R
@@ -8,20 +8,20 @@
 
 
 balanced.data<- function(year1, year2, unit){
-  
- m <- year2 - year1 + 1 
- 
- unique.unit<- unique(unit) 
- 
- year<- c(year1:year2) 
- year<- rep(year, length(unique.unit))
- 
- year<- sort(year) 
- 
- units<- rep(unique.units, m) 
- 
- dat<- cbind(year, units) 
-  
+     
+     m <- year2 - year1 + 1 
+     
+     unique.unit<- unique(unit) 
+     
+     year<- c(year1:year2) 
+     year<- rep(year, length(unique.unit))
+     
+     year<- sort(year) 
+     
+     units<- rep(unique.unit, m) 
+     
+     dat<- as.data.frame(cbind(year, units))
+     
 }
 
 # dat<- balanced.data(1995, 2016, ccode) 
